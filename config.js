edd5c229f47c9035fd9913e21227606821277652
cementownialagow-ctrl <cementownialagow@gmail.com>
2026-08-10 13:53:36 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 window.BETON_CONFIG={
-  API_URL:"https://TWOJ-BACKEND.onrender.com",
-  SUPABASE_URL:"https://TWOJ-PROJEKT.supabase.co",
-  SUPABASE_ANON_KEY:"TWOJ_SUPABASE_ANON_KEY"
+  API_URL:"https://cementownia-admin.onrender.com"
 };

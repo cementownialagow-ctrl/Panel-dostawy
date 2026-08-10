9883445a314f92a0e6772f6e7fbbda1edea7bbe1
cementownialagow-ctrl <cementownialagow@gmail.com>
2026-08-10 20:57:13 +0200

    Add files via upload

---
+++ panel-config.js
@@ -0,0 +1,5 @@
+// Osobny plik konfiguracji panelu kierowcy. Nazwa nie jest blokowana przez
+// rozszerzenia przeglądarki, które czasem zatrzymują ogólne "config.js".
+window.BETON_CONFIG={
+  API_URL:"https://cementownia-admin.onrender.com"
+};
